812805c78650c66fb742dc79af96851dadf80341
harrisdurrani <harris.durrani@hotmail.com>
2021-08-10 15:13:15 -0700

    converted date time values in df and changed tabs

--- analysis.py
+++ analysis.py
@@ -2,63 +2,70 @@ from codebase import code
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import statistics
+import matplotlib.pyplot as plt
 
 class Analysis:
 
-	def bollinger_band_analysis(self, stock_name:str):
-		candle_avgs = []
-		cb = code.Codebase()
-		auth_token = cb.get_access_token("authorization_code", cb.open_connection())
-		stock_data = cb.get_price_history(auth_token["access_token"], stock_name)
+    def bollinger_band_analysis(self, stock_name:str):
+        ''' runs analysis on the bollinger bands of stock data, checks within 2 standard deviations'''
+        candle_avgs = []
+        cb = code.Codebase() #import the codebase
+        auth_token = cb.get_access_token("authorization_code", cb.open_connection()) #get auth code
+        stock_data = cb.get_price_history(auth_token["access_token"], stock_name) #get stock history
 
-		std_deviations = 2
-		upper_band = None
-		lower_band = None
+        std_deviations = 2
+        upper_band = None
+        lower_band = None
 
-		for candle in stock_data.get("candles"):
-			candle_avgs.append(((int(candle["high"]) + int(candle["low"])) / 2))
+        for candle in stock_data.get("candles"):
+            candle_avgs.append(((int(candle["high"]) + int(candle["low"])) / 2))
 
-		moving_avg = (sum(candle_avgs) / len(candle_avgs))
-		
-		upper_band = moving_avg + (std_deviations * statistics.stdev(candle_avgs))
-		lower_band = moving_avg - (std_deviations * statistics.stdev(candle_avgs))
+        moving_avg = (sum(candle_avgs) / len(candle_avgs)) #calculate the moving average between the candles
+        
+        upper_band = moving_avg + (std_deviations * statistics.stdev(candle_avgs)) #calculate the upper band
+        lower_band = moving_avg - (std_deviations * statistics.stdev(candle_avgs)) #calculate the lower band
 
 
-		return f"stock name: {stock_name},\n upper band: {upper_band},\n moving average: {moving_avg},\n lower band: {lower_band}"
+        return f"stock name: {stock_name},\n upper band: {upper_band},\n moving average: {moving_avg},\n lower band: {lower_band}"
 
-		# print("#" * 10)
-		# print(stock_name)
-		# print(upper_band)
-		# print(moving_avg)
-		
-		# print(lower_band)
-		
-		# print("#" * 10)
-		
-	def convert_to_df(self, stock_name:str):
-		cb = code.Codebase()
-		auth_token = cb.get_access_token("authorization_code", cb.open_connection())
-		stock_data = cb.get_price_history(auth_token["access_token"], stock_name)
+        # print("#" * 10)
+        # print(stock_name)
+        # print(upper_band)
+        # print(moving_avg)
+        
+        # print(lower_band)
+        
+        # print("#" * 10)
+        
+    def convert_to_df(self, stock_name:str):
+        cb = code.Codebase() #call the codebase class
+        auth_token = cb.get_access_token("authorization_code", cb.open_connection()) #get auth token
+        stock_data = cb.get_price_history(auth_token["access_token"], stock_name) #get stock data and its history
 
-		points = stock_data['candles']
+        points = stock_data['candles']
 
-		df = pd.json_normalize(points)
+        df = pd.json_normalize(points) #converts the json stock data to a pandas df
 
-		return df
+        df['datetime'] = pd.to_datetime(df['datetime'], unit = 'ms') #converts date time column from epoch to human readable time
+        return df
 
-	def regression_analysis(self, stock_name:str):
-		pass
+    def regression_analysis(self, stock_name:str):
+        pass
 
 
 
 
 if __name__ == "__main__":
-	stocks = ["KO"]
-	analysis = Analysis()
-	print(analysis.convert_to_df("KO"))
+    stocks = ["KO"]
+    analysis = Analysis()
+    stock = analysis.convert_to_df("KO")
 
-	# for i in stocks:
+    plt.figure(figsize=(16,8))
+    plt.plot(stock['close'], label = 'Close price history')
+    plt.show()
 
-	# 	print(analysis.bollinger_band_analysis(i))
+    # for i in stocks:
+
+    # 	print(analysis.bollinger_band_analysis(i))
 
 
--- codebase/code.py
+++ codebase/code.py
@@ -7,94 +7,94 @@ import sys
 
 class Codebase:
 
-	def __init__(self):
-		self.client_code = os.getenv("API_KEY")
-		self.username = os.getenv("USERNAME")
-		self.pwd = os.getenv("PWD")
-		self.sec1 = os.getenv("SEC1")
-		self.sec2 = os.getenv("SEC2")
-		self.sec3 = os.getenv("SEC3")
-		self.sec4 = os.getenv("SEC4")
-		self.redirect_uri = 'https://localhost:8000'
-		self.base_url = "https://api.tdameritrade.com/v1"
+    def __init__(self):
+        self.client_code = os.getenv("API_KEY")
+        self.username = os.getenv("USERNAME")
+        self.pwd = os.getenv("PWD")
+        self.sec1 = os.getenv("SEC1")
+        self.sec2 = os.getenv("SEC2")
+        self.sec3 = os.getenv("SEC3")
+        self.sec4 = os.getenv("SEC4")
+        self.redirect_uri = 'https://localhost:8000'
+        self.base_url = "https://api.tdameritrade.com/v1"
 
 
 
-	def open_connection(self):
-		executable_path =  r'D:\PY Files\My Projects\stockanalysis\dependencies\chromedriver'
-		browser = Browser('chrome', executable_path=executable_path, headless = False)
+    def open_connection(self):
+        executable_path =  r'D:\PY Files\My Projects\stockanalysis\dependencies\chromedriver'
+        browser = Browser('chrome', executable_path=executable_path, headless = False)
 
-		method = 'GET'
-		url = 'https://auth.tdameritrade.com/auth?'
-		payload = {'response_type': 'code', 'redirect_uri': self.redirect_uri, 'client_id': self.client_code}
+        method = 'GET'
+        url = 'https://auth.tdameritrade.com/auth?'
+        payload = {'response_type': 'code', 'redirect_uri': self.redirect_uri, 'client_id': self.client_code}
 
-		built_url = requests.Request(method, url, params= payload).prepare()
-		# built_url = requests.get(url, params= payload)
-		# print(built_url)
-		# response = requests.post(method, url, params= payload)
+        built_url = requests.Request(method, url, params= payload).prepare()
+        # built_url = requests.get(url, params= payload)
+        # print(built_url)
+        # response = requests.post(method, url, params= payload)
 
-		built_url = built_url.url
-		browser.visit(built_url)
+        built_url = built_url.url
+        browser.visit(built_url)
 
-		browser.find_by_id("username0").first.fill(self.username)
-		browser.find_by_id("password1").first.fill(self.pwd)
-		browser.find_by_id("accept").first.click()
-
-
-		browser.find_by_xpath('//*[@id="authform"]/main/details/summary').click()
-		browser.find_by_name('init_secretquestion').click()
+        browser.find_by_id("username0").first.fill(self.username)
+        browser.find_by_id("password1").first.fill(self.pwd)
+        browser.find_by_id("accept").first.click()
 
 
-		if browser.is_text_present("In what city was your high school? (Enter full name of city only.)"):
-			browser.find_by_name("su_secretquestion").first.fill(self.sec1)
-		elif browser.is_text_present("In what city were you born?"):
-			browser.find_by_name("su_secretquestion").first.fill(self.sec2)
-		elif browser.is_text_present("What was your high school mascot?"):
-			browser.find_by_name("su_secretquestion").first.fill(self.sec3)
-		else:
-			browser.find_by_name("su_secretquestion").first.fill(self.sec4)
-
-		browser.find_by_xpath('//*[@id="authform"]/main/div[5]/span/label').first.click()
-
-		browser.find_by_id("accept").first.click()
-		browser.find_by_id("accept").first.click()
-		time.sleep(1)
-		new_url = browser.url
-		parse_url = urllib.parse.unquote(new_url.split('code=')[1])
-		browser.quit()
+        browser.find_by_xpath('//*[@id="authform"]/main/details/summary').click()
+        browser.find_by_name('init_secretquestion').click()
 
 
-		return parse_url
+        if browser.is_text_present("In what city was your high school? (Enter full name of city only.)"):
+            browser.find_by_name("su_secretquestion").first.fill(self.sec1)
+        elif browser.is_text_present("In what city were you born?"):
+            browser.find_by_name("su_secretquestion").first.fill(self.sec2)
+        elif browser.is_text_present("What was your high school mascot?"):
+            browser.find_by_name("su_secretquestion").first.fill(self.sec3)
+        else:
+            browser.find_by_name("su_secretquestion").first.fill(self.sec4)
+
+        browser.find_by_xpath('//*[@id="authform"]/main/div[5]/span/label').first.click()
+
+        browser.find_by_id("accept").first.click()
+        browser.find_by_id("accept").first.click()
+        time.sleep(1)
+        new_url = browser.url
+        parse_url = urllib.parse.unquote(new_url.split('code=')[1])
+        browser.quit()
 
 
-	def get_access_token(self, grant_type, code, access_type="offline"):
-		
-		''' refer to the post access token call on the ameritrade api documentation to get param 
-		to get code variable, pass in open_connection method (return value is code)'''
-		url = self.base_url + "/oauth2/token"
-		payload = {"grant_type": grant_type, "access_type": access_type, "code": code, "client_id": self.client_code, "redirect_uri": self.redirect_uri}
-
-		r = requests.post(url, data= payload)
-
-		return r.json()
+        return parse_url
 
 
-	def get_stock_quote(self, access_token, symbol):
+    def get_access_token(self, grant_type, code, access_type="offline"):
+        
+        ''' refer to the post access token call on the ameritrade api documentation to get param 
+        to get code variable, pass in open_connection method (return value is code)'''
+        url = self.base_url + "/oauth2/token"
+        payload = {"grant_type": grant_type, "access_type": access_type, "code": code, "client_id": self.client_code, "redirect_uri": self.redirect_uri}
 
-		url = self.base_url + f"/marketdata/{symbol}/quotes"
-		params = {"apikey": self.client_code}
-		headers = {"Authorization": f"Bearer {access_token}"}
+        r = requests.post(url, data= payload)
 
-		r = requests.get(url, params = params, headers = headers)
-		return r.json()
+        return r.json()
 
-	def get_price_history(self, access_token, symbol, period_type="day", period="10"):
-		url = self.base_url + f"/marketdata/{symbol}/pricehistory"
-		params = {"apikey": self.client_code, "periodType": period_type, "period": period}
-		headers = {"Authorization": f"Bearer {access_token}"}
 
-		r = requests.get(url, params = params, headers = headers)
-		return r.json()
+    def get_stock_quote(self, access_token, symbol):
+
+        url = self.base_url + f"/marketdata/{symbol}/quotes"
+        params = {"apikey": self.client_code}
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        r = requests.get(url, params = params, headers = headers)
+        return r.json()
+
+    def get_price_history(self, access_token, symbol, period_type="day", period="10"):
+        url = self.base_url + f"/marketdata/{symbol}/pricehistory"
+        params = {"apikey": self.client_code, "periodType": period_type, "period": period}
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        r = requests.get(url, params = params, headers = headers)
+        return r.json()
 
 
 # cb = Codebase()
